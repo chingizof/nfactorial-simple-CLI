6969beaeae5425f867fe2c5907b8a8f4ea9ebbf4
Mister-718 <59221853+Mister-718@users.noreply.github.com>
2022-06-07 18:59:08 +0600

    request price for any cryptocurrency

--- commands/btc.js
+++ commands/btc.js
@@ -1,11 +1,11 @@
 const conf = new (require('conf'))()
 const axios = require('axios');
 
-function btc () {
+function btc (coin) {
     let response = null;
     new Promise(async (resolve, reject) => {
     try {
-        response = await axios.get('https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/price-performance-stats/latest?id=1', {
+        response = await axios.get(`https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/price-performance-stats/latest?id=${coin}`, {
         headers: {
             'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
         },
@@ -20,8 +20,8 @@ function btc () {
     if (response) {
         // success
         const json = response.data;
-        console.log("Current BTC price is :")
-        console.log(json.data['1'].quote.USD.open)
+        console.log(`Current price of token with id ${coin} is :`)
+        console.log(json.data[coin].quote.USD.open)
         resolve();
     }
     });
